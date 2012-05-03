1d2a8d9e30b54afe522e3998b6ce3f5bf66516be
Ken Roberts <ken@9ci.com>
2012-05-03 15:27:20 -0500

    Added ContactFlex

---
+++ grails-app/domain/nine/rally/ContactFlex.groovy
@@ -0,0 +1,47 @@
+package nine.rally
+
+class ContactFlex {
+	String text1
+	String text2
+	String text3
+	String text4
+	String text5
+	String text6
+	String text7
+	String text8
+	String text9
+	String text10
+	Date date1
+	Date date2
+	Date date3
+	Date date4
+	BigDecimal num1
+	BigDecimal num2
+	BigDecimal num3
+	BigDecimal num4
+	BigDecimal num5
+	BigDecimal num6
+	
+    static constraints = {
+		text1 (nullable:true)
+		text2 (nullable:true)
+		text3 (nullable:true)
+		text4 (nullable:true)
+		text5 (nullable:true)
+		text6 (nullable:true)
+		text7 (nullable:true)
+		text8 (nullable:true)
+		text9 (nullable:true)
+		text10 (nullable:true)
+		date1 (nullable:true)
+		date2 (nullable:true)
+		date3 (nullable:true)
+		date4 (nullable:true)
+		num1 (nullable:true)
+		num2 (nullable:true)
+		num3 (nullable:true)
+		num4 (nullable:true)
+		num5 (nullable:true)
+		num6 (nullable:true)
+    }
+}
